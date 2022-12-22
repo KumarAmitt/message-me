854e421eb89cffdf11437ad627dee21b80b22e74
KumarAmitt <amitt2015@outlook.com>
2022-12-22 15:02:00 +0530

    Add user authentication

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,9 @@
 
 $(document).on('turbolinks:load', () => {
   $('.ui.dropdown').dropdown();
+
+  $('.message .close').on('click', function() {
+        $(this).closest('.message').transition('fade');
+  });
+
 })
